567eda38aea548cb0f02d8c1c2165fc9282090e6
Sam Hall <github@solflux.co.uk>
2020-05-26 20:32:27 +0100

    :sparkles: Added basic support for taxonomies

---
+++ src/Taxonomy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Solflux\WordPress;
+
+class Taxonomy
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var bool
+     */
+    private $public = true;
+
+    /**
+     * @var bool
+     */
+    private $hierarchical = false;
+
+    /**
+     * Taxonomy constructor.
+     *
+     * @param string $name
+     * @param string $type
+     * @param string $label
+     */
+    public function __construct(string $name, string $type, string $label)
+    {
+        $this->name  = $name;
+        $this->type  = $type;
+        $this->label = $label;
+    }
+
+    /**
+     * @param bool $public
+     *
+     * @return Taxonomy
+     */
+    public function setPublic(bool $public): Taxonomy
+    {
+        $this->public = $public;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $hierarchical
+     *
+     * @return Taxonomy
+     */
+    public function setHierarchical(bool $hierarchical): Taxonomy
+    {
+        $this->hierarchical = $hierarchical;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'label' => $this->label,
+            'public' => $this->public,
+            'hierarchical' =>  $this->hierarchical
+        ];
+    }
+
+}
--- src/WordPress.php
+++ src/WordPress.php
@@ -4,6 +4,7 @@ namespace Solflux\WordPress;
 
 use function add_action;
 use function register_post_type;
+use function register_taxonomy;
 
 class WordPress
 {
@@ -13,6 +14,15 @@ class WordPress
             Hooks::PLUGIN_INITIALIZATION,
             function () use ($type) {
                 register_post_type($type->getName(), $type->asArray());
+            }
+        );
+    }
+
+    public function registerTaxonomy(Taxonomy $taxonomy) {
+        add_action(
+            Hooks::PLUGIN_INITIALIZATION,
+            function() use ($taxonomy) {
+                register_taxonomy($taxonomy->getName(), $taxonomy->getType(), $taxonomy->toArray());
             }
         );
     }
