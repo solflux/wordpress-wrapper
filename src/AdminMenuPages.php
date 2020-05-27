7dae638e998ded1149a556374fdf1d7295ed3ab1
Sam Hall <github@solflux.co.uk>
2020-05-27 01:28:29 +0100

    :sparkles: Added a collection of const to represent the slugs of existing top-levl admin menu pages

---
+++ src/AdminMenuPages.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Solflux\WordPress;
+
+class AdminMenuPages
+{
+    const DASHBOARD = 'index.php';
+    const POSTS = 'edit.php';
+    const MEDIA = 'upload.php';
+    const LINKS = 'link-manager.php';
+    const PAGES = 'edit.php?post_type=page';
+    const COMMENTS = 'edit-comments.php';
+    const TOOLS = 'tools.php';
+    const OPTIONS = 'options-general.php';
+    const THEME = 'themes.php';
+    const PLUGINS = 'plugins.php';
+}
